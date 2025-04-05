e77ea62bc9a8d922c96aed9796709c1de73a1a9e
iamshivamgaur <shivam24august@gmail.com>
2025-04-06 02:34:08 +0530

    inti

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="w-full h-screen p-10">
+      <div>
+        {/* lef div */}
+        <div>
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+            adipisci.
+          </h1>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+          <button>Get started</button>
+        </div>
+        {/* right div */}
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
